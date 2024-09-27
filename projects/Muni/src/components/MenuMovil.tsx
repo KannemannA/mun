2dafb68d17ba99c78911432d1fcc8f65f383347b
Alejo.K <alejokannemann@gmail.com>
2024-09-26 21:00:27 -0300

    lss

--- projects/Muni/src/components/MenuMovil.tsx
+++ projects/Muni/src/components/MenuMovil.tsx
@@ -20,9 +20,16 @@ const InteractiveHeader: React.FC = () => {
     <>
       {/* Encabezado móvil */}
       <div className="lg:hidden flex justify-between items-center p-4">
-        <div className="flex items-center">
-          <img src="/src/assets/img/LOGO-ISOTIPO-MUNI-BLANCO-2.svg" alt="Logo del Municipio" className="h-8 w-8 mr-2" />
-        </div>
+        <a href="https://www.iguazu.gob.ar" className="flex items-center">
+    <img
+        width="604"
+        height="181"
+        src="https://www.iguazu.gob.ar/wp-content/uploads/2021/09/LOGO-ISOTIPO-MUNI-BLANCO-2.svg"
+        alt=""
+        className="w-full h-auto"
+        loading="lazy"
+    />
+</a>
         <button onClick={toggleMobileMenu} className="text-white">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
