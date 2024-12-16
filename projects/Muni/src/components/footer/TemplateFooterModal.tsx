60a1a2e8ab2c10dc0acf797037918db60caf18ec
Alejo.K <alejokannemann@gmail.com>
2024-12-16 17:50:25 -0300

    footer terminado + links header

--- projects/Muni/src/components/footer/TemplateFooterModal.tsx
+++ projects/Muni/src/components/footer/TemplateFooterModal.tsx
@@ -8,9 +8,9 @@ const TemplateFooterModal: React.FC<TemplateModalProps> = ({children, isOpen, cl
 
   return (
     <section className="text-[#333333]">
-      <span onClick={closeModal} className={`size-[50px] bg-green-700 fixed top-2 right-4 z-50 rounded-full flex justify-center items-center hover:bg-green-600 hover:cursor-pointer ${isOpen ? "visible" : "invisible"} transition-all duration-300`}><i className="fa-solid fa-xmark text-[38px] text-white"></i></span>
-      <div onClick={closeModal} className={`fixed inset-0 z-40 bg-black opacity-80 ${isOpen ? "visible" : "invisible"} transition-all duration-300`}></div>
-      <div className={`fixed m-auto w-[70%] h-screen inset-0 z-50 box-border rounded-md bg-white max-h-[620px] lg:max-h-[820px] lg:max-w-[800px] transition-all duration-500 ${isOpen ? 'scale-100' : 'scale-0'} ${isOpen ? "visible" : "invisible"}`}>
+      <span onClick={closeModal} className={`size-[50px] bg-green-700 fixed top-2 right-4 z-20 rounded-full flex justify-center items-center hover:bg-green-600 hover:cursor-pointer ${isOpen ? "visible" : "invisible"} transition-all duration-500 ease-in-out`}><i className="fa-solid fa-xmark text-[38px] text-white"></i></span>
+      <div onClick={closeModal} className={`fixed inset-0 z-10 bg-black opacity-80 ${isOpen ? "visible" : "invisible"} transition-all duration-500 ease-in-out`}></div>
+      <div className={`fixed m-auto w-screen md:w-[70%] h-screen inset-0 z-10 box-border rounded-md bg-white lg:max-h-[820px] lg:max-w-[800px] transition-all duration-700 scale-100 md:translate-y-0 ${isOpen ? 'translate-y-0' : 'translate-y-[100vh]'} ${isOpen ? 'md:scale-100' : 'md:scale-0'} ${isOpen ? "visible" : "invisible"} ease-in-out overflow-auto`}>
         {children}
       </div>
 </section>
--- projects/Muni/src/sections/ModalPortal.tsx
+++ projects/Muni/src/sections/ModalPortal.tsx
@@ -23,9 +23,13 @@ const ModalPortal: React.FC<ModalPortalProps> = ({trigger, content}) => {
   };
 
   const menuLista = ['La Ciudad', 'La Municipalidad', 'Trámites'];
-  const ciudadLista = ["El Intendente", "Secretaría de Gobierno", "Reseña Histórica", "Dirección de Patrimonio Histórico"];
-  const muniLista = ['Turismo', 'Cultura', 'Deportes', 'Medio Ambiente', 'Dirección de la Juventud', 'Oficina de Empleo', 'Diversidad y Géneros', 'Higiene y Seguridad', 'Bromatología', 'Tránsito', 'Zoonosis', 'Vectores'];
-  const tramiteLista = ['Trámites de tránsito', 'Trámites de bromatología', 'Trámites del cementerio', 'Trámites de pensión por invalidez', 'Trámites de fiscalización'];
+  const ciudadLista = [["El Intendente", "https://www.iguazu.gob.ar/intendente/"], ["Secretaría de Gobierno", "https://www.iguazu.gob.ar/secretaria-de-gobierno/"], ["Reseña Histórica", "https://www.iguazu.gob.ar/historia/"], ["Dirección de Patrimonio Histórico", "https://historiadeiguazu.org/", "_blank"]];
+  const muniLista = [['Turismo', "https://visitiguazu.travel/", "_blank"], ['Cultura', "https://www.iguazu.gob.ar/cultura/"], ['Deportes', "https://www.iguazu.gob.ar/deportes-2/"], ['Medio Ambiente', "https://www.iguazu.gob.ar/medio-ambiente/"], ['Dirección de la Juventud', "https://www.iguazu.gob.ar/direccion-de-la-juventud/"], ['Oficina de Empleo', "https://www.iguazu.gob.ar/oficina-de-empleo/"], ['Diversidad y Géneros', "https://www.iguazu.gob.ar/diversidadygeneros/"], ['Higiene y Seguridad', "https://www.iguazu.gob.ar/higiene-y-seguridad/"], ['Bromatología', "https://www.iguazu.gob.ar/bromatologia/"], ['Tránsito', "https://www.iguazu.gob.ar/transito/"], ['Zoonosis', "https://www.iguazu.gob.ar/zoonosis/"], ['Vectores', "https://www.iguazu.gob.ar/vectores/"]];
+  const tramiteLista = [['Trámites de tránsito', "https://www.iguazu.gob.ar/tramites-de-transito/"], ['Trámites de bromatología', "https://www.iguazu.gob.ar/tramites-de-bromatologia/"], ['Trámites del cementerio', "https://www.iguazu.gob.ar/tramites-del-cementerio/"], ['Trámites de pensión por invalidez', "https://www.iguazu.gob.ar/tramites-de-pension-por-invalidez/"], ['Trámites de fiscalización', "https://www.iguazu.gob.ar/tramites-de-fiscalizacion/"]];
+  const horarioLista = ["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado", "Domingo"];
+  const telefonoLista = [["Dirección de Cultura", "Tel: (03757) 415-358"],["Cementerio", "Tel: (03757) 423-007"], ["Iguazú Turismo Ente Municipal (ITUREM)", "Tel: (03757) 423-951"], ["Oficina de Empleo", "Tel: (03757) 421-515"], ["Rentas de Misiones", "Tel: (03757) 422-649"], ["Honorable Concejo Deliberante", "Tel: (03757) 421-518"], ["Juzgado de Faltas", "Únicamente atención presencial."]];
+  const bromatologíaLista = [["Libreta Sanitaria", "Libreta Sanitaria.pdf"], ["Examen Médico (Anexo)", "Examen Médico (Anexo).pdf"]];
+  const fiscalizaciónLista = [["Solicitud de Habilitación e Inscripción o Traslado", "Solicitud de Habilitación e Inscripción o Traslado.pdf"], ["Solicitud de Habilitación e Inscripción Profesional", "Solicitud de Habilitación e Inscripción Profesional.pdf"], ["Solic. de Hab. e Inscrip. de Farm., Clín., San., C. de Salud y Lab.", "Solicitud de Habilitación e Inscripción de Farmacias, Clínicas, Sanatorios, Centros de Salud y Laboratorios.pdf"], ["Solic. de Hab. e Inscripción de Parada de Remises", "Solicitud de Habilitación e Inscripción de Parada de Remises.pdf"], ["Solicitud de Habilitación e Inscripción de Hoteles", "Solicitud de Habilitación e Inscripción de Hoteles.pdf"], ["Solic. de Hab. e Inscripción de Empresas Constructoras", "Solicitud de Habilitación e Inscripción de Empresas Constructoras.pdf"]];
 
   const triggerOptions = {
     "menu movil":
@@ -48,7 +52,7 @@ const ModalPortal: React.FC<ModalPortalProps> = ({trigger, content}) => {
     "menu movil":
     <>
       <div className={`fixed inset-0 bg-black opacity-5 z-10 ${isOpen ? "visible" : "invisible"}`} onClick={closeModal}></div>
-      <ul className={`bg-white text-black fixed top-0 left-0 pt-5 w-[250px] md:w-[300px] h-screen md:text-[13px] text-[15px] overflow-auto z-20 ${isOpen ? "visible" : "invisible"}`}>
+      <ul className={`bg-white text-black fixed top-0 left-0 pt-5 w-[250px] md:w-[300px] h-screen md:text-[13px] text-[15px] overflow-auto z-20 transition-all duration-500 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-[40vw]"}`}>
         {menuLista.map((item) => (
           <React.Fragment key={item}>
             <li key={item} onClick={() => toggleDropdown(item)} className={`flex items-center justify-between w-full md:h-[43.66px] hover:bg-[#019f48] hover:text-white px-5 ${openDropdowns.includes(item) ? '' : 'border-b border-gray-300'} h-[47.3px]`}>
@@ -57,52 +61,99 @@ const ModalPortal: React.FC<ModalPortalProps> = ({trigger, content}) => {
             </li>
             <ul className={`${openDropdowns.includes(item) ? '' : 'hidden'}`}>
               {item==='La Ciudad' && ciudadLista.map((art) => (
-                <li key={art} className='h-[40.0938px] border-b border-gray-300 flex items-center px-5 text-[11.05px] hover:bg-[#019f48] hover:text-white'>
-                  <a href="">
-                    {art}
+                <li key={art[0]}>
+                  <a href={art[1]} target={`${art[2] ? art[2] : "_self"}`} className='h-[40.0938px] border-b border-gray-300 flex items-center px-5 text-[11.05px] hover:bg-[#019f48] hover:text-white' >
+                    {art[0]}
                   </a>
                 </li>
               ))}
               {item==='La Municipalidad' && muniLista.map((art) => (
-                <li key={art} className='h-[40.0938px] border-b border-gray-300 flex items-center px-5 text-[11.05px] hover:bg-[#019f48] hover:text-white'>
-                  <a href="">
-                    {art}
-                  </a>
-                </li>
+                <li key={art[0]}>
+                <a href={art[1]} target={`${art[2] ? art[2] : "_self"}`} className='h-[40.0938px] border-b border-gray-300 flex items-center px-5 text-[11.05px] hover:bg-[#019f48] hover:text-white' >
+                  {art[0]}
+                </a>
+              </li>
               ))}
               {item==='Trámites' && tramiteLista.map((art) => (
-                <li key={art} className='h-[40.0938px] border-b border-gray-300 flex items-center px-5 text-[11.05px] hover:bg-[#019f48] hover:text-white'>
-                  <a href="">
-                    {art}
-                  </a>
-                </li>
+                <li key={art[0]}>
+                <a href={art[1]} target={`${art[2] ? art[2] : "_self"}`} className='h-[40.0938px] border-b border-gray-300 flex items-center px-5 text-[11.05px] hover:bg-[#019f48] hover:text-white' >
+                  {art[0]}
+                </a>
+              </li>
               ))}
             </ul>
           </React.Fragment>
         ))}
-        <li className="flex items-center justify-between w-full md:h-[43.66px] hover:bg-[#019f48] hover:text-white px-5 border-b border-gray-300 h-[47.3px]"><a href="#">Webmail</a></li>
-        <li className="flex items-center justify-between w-full md:h-[43.66px] hover:bg-[#019f48] hover:text-white px-5 h-[47.3px]"><a href="#">Contacto</a></li>
+        <li><a className='flex items-center justify-between w-full md:h-[43.66px] hover:bg-[#019f48] hover:text-white px-5 border-b border-gray-300 h-[47.3px]' href="https://vps-1656188-x.dattaweb.com:2094/login.php?idn">Webmail</a></li>
+        <li><a className="flex items-center justify-between w-full md:h-[43.66px] hover:bg-[#019f48] hover:text-white px-5 h-[47.3px]" href="https://www.iguazu.gob.ar/contacto/">Contacto</a></li>
       </ul>
     </>,
     "Cómo llegar": 
     <>
-      <span onClick={closeModal} className={`size-[50px] bg-green-700 fixed top-2 right-4 z-50 rounded-full flex justify-center items-center hover:bg-green-600 hover:cursor-pointer ${isOpen ? "visible" : "invisible"} transition-all duration-300`}><i className="fa-solid fa-xmark text-[38px] text-white"></i></span>
-      <div onClick={closeModal} className={`fixed inset-0 z-40 bg-black opacity-80 ${isOpen ? "visible" : "invisible"} transition-all duration-300`}></div>
-      <div className={`fixed m-auto w-[70%] h-screen inset-0 z-50 p-[10px] box-border rounded-md bg-white max-h-[620px] lg:max-h-[820px] lg:max-w-[800px] transition-all duration-500 ${isOpen ? 'scale-100' : 'scale-0'} ${isOpen ? "visible" : "invisible"}`}>
+      <span onClick={closeModal} className={`size-[50px] bg-green-700 fixed top-2 right-4 z-50 rounded-full flex justify-center items-center hover:bg-green-600 hover:cursor-pointer ${isOpen ? "visible" : "invisible"} transition-all duration-500`}><i className="fa-solid fa-xmark text-[38px] text-white"></i></span>
+      <div onClick={closeModal} className={`fixed inset-0 z-40 bg-black opacity-80 ${isOpen ? "visible" : "invisible"} transition-all duration-500`}></div>
+      <div className={`fixed m-auto w-[70%] h-screen inset-0 z-50 p-[10px] box-border rounded-md bg-white max-h-[620px] lg:max-h-[820px] lg:max-w-[800px] transition-all duration-700 ${isOpen ? 'scale-100' : 'scale-0'} ${isOpen ? "visible" : "invisible"}`}>
         <iframe className='size-full' src="https://maps.google.com/maps?q=Puerto%20Iguaz%C3%BA%2C%20Misiones&amp;t=m&amp;z=12&amp;output=embed&amp;iwloc=near" title="Puerto Iguazú, Misiones" aria-label="Puerto Iguazú, Misiones"></iframe>
       </div>
     </>,
     "Horarios de atención":
       <TemplateFooterModal isOpen={isOpen} closeModal={closeModal}>
         <AvisoFooterModal content='Hemos vuelto a normalizar los horarios de atención al público.' />
+        <h2 className='p-[10px] ml-5 text-4xl'>Nuestros horarios de atención</h2>
+        <ul className='p-[30px] ml-5'>
+          {horarioLista.map((item, index) => (
+            <li key={item} className={`${index === horarioLista.length-1 ? "" :"mb-[35px]"}`}>
+              <div className='flex mb-[10px] items-center'>
+                <span className='font-medium text-[19px]'>{item}</span>
+                <span className='flex-grow border-b h-0 mx-[10px]'></span>
+              </div>
+              {index >= horarioLista.length - 2 ? <p className='text-sm text-[#707070]'>No atendemos</p> : <p className='text-sm text-[#707070]'>De 7:30 a 12:30 horas</p>}
+            </li>
+          ))}
+        </ul>
       </TemplateFooterModal>,
     "Teléfonos útiles":
     <TemplateFooterModal isOpen={isOpen} closeModal={closeModal}>
-        <h1>text</h1>
+        <h2 className='p-[10px] ml-5 text-4xl mt-5'>Teléfonos útiles</h2>
+        <ul className='p-[30px] ml-5'>
+          {telefonoLista.map((item, index) => (
+            <li key={item[0]} className={`${index === horarioLista.length-1 ? "" :"mb-[35px]"}`}>
+              <div className='flex mb-[10px] items-center'>
+                <span className='font-medium text-[19px]'>{item[0]}</span>
+                <span className='flex-grow border-b h-0 mx-[10px]'></span>
+              </div>
+              <p className='text-sm text-[#707070]'>{item[1]}</p>
+            </li>
+          ))}
+        </ul>
       </TemplateFooterModal>,
     "Formularios y documentos":
     <TemplateFooterModal isOpen={isOpen} closeModal={closeModal}>
         <AvisoFooterModal content='Una vez que hayas rellenado el formulario, acércate a nuestras oficinas (en el Iturem) para concretar el trámite.' />
+        <h2 className='p-[10px] ml-5 text-4xl'>Bromatología</h2>
+        <ul className='p-[30px] ml-5'>
+        {bromatologíaLista.map((item, index) => (
+            <li key={item[0]} className={`${index === bromatologíaLista.length-1 ? "" :"mb-[35px]"}`}>
+              <div className='flex mb-[10px] items-center'>
+                <span className='font-medium text-[19px]'>{item[0]}</span>
+                <span className='flex-grow border-b h-0 mx-[10px]'></span>
+              </div>
+              <p className='text-sm text-[#707070]'>{item[1]}</p>
+            </li>
+          ))}
+        </ul>
+        <h2 className='p-[10px] ml-5 text-4xl'>Fiscalización</h2>
+        <ul className='p-[30px] ml-5'>
+        {fiscalizaciónLista.map((item, index) => (
+            <li key={item[0]} className={`${index === fiscalizaciónLista.length-1 ? "" :"mb-[35px]"}`}>
+              <div className='flex mb-[10px] items-center'>
+                <span className='font-medium text-[19px] max-w-[410px]'>{item[0]}</span>
+                <span className='flex-grow border-b h-0 mx-[10px]'></span>
+              </div>
+              <p className='text-sm text-[#707070]'>{item[1]}</p>
+            </li>
+          ))}
+        </ul>
       </TemplateFooterModal>
   }
 
