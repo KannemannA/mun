e9d15c16616c93140dff03f0f39de86997d48e5f
Alejo.K <alejokannemann@gmail.com>
2024-12-17 17:58:10 -0300

    mod settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  base: 'page',
+  base: 'mun',
   site: "https://kannemanna.github.io"
 });
